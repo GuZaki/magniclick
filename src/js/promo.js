8d4efe35486e89fcb695933858b972862621e115
Guzel <viproo@bk.ru>
2025-01-07 19:00:53 +0300

    adding sections

---
+++ src/js/promo.js
@@ -0,0 +1,11 @@
+// 'use strict';
+
+// const img = document.querySelector(".bottom-menu__img");
+
+// img.addEventListener("mouseover", function () {
+//   this.src = "../images/promo/team-hover.svg"; // Меняем картинку при наведении
+// });
+
+// img.addEventListener("mouseout", function () {
+//   this.src = "../images/promo/team.svg"; // Возвращаем исходную картинку при уходе с области
+// });
